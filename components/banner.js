a8c238576663ecec00ca66d8a5d7659036db706b
LeeHairDev <leehairdev.it@gmail.com>
2023-03-21 19:04:01 +0700

    update code

--- components/banner.js
+++ components/banner.js
@@ -19,7 +19,7 @@ const banner = () => {
 </div>
 <div class="image-profile col-sm-4">
  <div class="image-profile-mini col-sm-4">
- <img src="img/z4122535271404_d5a207183fb5ca0f9878d80ebb5e4c4a.jpg" alt="">
+ <img src="../img/z4122535271404_d5a207183fb5ca0f9878d80ebb5e4c4a.jpg" alt="">
 </div>
    <span class="border-after"></span>
 </div>
